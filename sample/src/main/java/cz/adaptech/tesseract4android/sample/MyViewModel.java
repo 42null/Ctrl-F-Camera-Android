c637f78ee2c8e35f007b830ae3ac73e8a2b0d311
42null <averyparanoiduser@gmail.com>
2023-10-19 22:46:09 -0500

    Editible search settings semi-persistent options

--- sample/src/main/java/cz/adaptech/tesseract4android/sample/MyViewModel.java
+++ sample/src/main/java/cz/adaptech/tesseract4android/sample/MyViewModel.java
@@ -4,10 +4,20 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyViewModel extends ViewModel {
     private MutableLiveData<Boolean> showPreProcessing = new MutableLiveData<>();
     private MutableLiveData<String> stringAllTextFromLastDetection = new MutableLiveData<>("");
 
+    private Map<String, Boolean> settingsStateMap = new HashMap<>() {{
+        put("\"(\" = \"C\"", true);
+        put("Ignore Capitalization", false);
+    }};
+
+
+//Preprocess
     public void setBooleanCheckboxShowPreProcessing(boolean value) {
         showPreProcessing.setValue(value);
     }
@@ -15,12 +25,24 @@ public class MyViewModel extends ViewModel {
         return showPreProcessing;
     }
 
-
+//Last Text
     public void setAllTextFromLastDetection(String value) {
         stringAllTextFromLastDetection.postValue(value);
     }
     public String getAllTextFromLastDetection() {
         return stringAllTextFromLastDetection.getValue();
     }
+
+
+//Search Settings
+    public void setSettingsStateMap(Map<String, Boolean> settingsMap) {
+        this.settingsStateMap = settingsMap;
+    }
+    public Map<String, Boolean> getSettingsStateMap() {
+        return settingsStateMap;
+    }
+
+
+
 
 }
--- sample/src/main/java/cz/adaptech/tesseract4android/sample/SearchFilteringDialogFragment.java
+++ sample/src/main/java/cz/adaptech/tesseract4android/sample/SearchFilteringDialogFragment.java
@@ -4,58 +4,114 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SearchFilteringDialogFragment extends DialogFragment {
 
+    private MyViewModel viewModel;
+
     private String persistentText = ""; //TODO: Make save in viewModel true persistence
+    Map<String, Boolean> localSearchOptionsHashMap;
+
+//    Options
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(MyViewModel.class);
+    }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        localSearchOptionsHashMap = viewModel.getSettingsStateMap(); //Get latest
 
         // Use the Builder class for convenient dialog construction.
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        View customTitleView = getLayoutInflater().inflate(R.layout.centered_text_view, null);
-        builder.setCustomTitle(customTitleView);
+//        View customTitleView = getLayoutInflater().inflate(R.layout.edit_search_parameters_dialog, null);
+//        builder.setCustomTitle(customTitleView);
+//        TextView titleTextView = customTitleView.findViewById(R.id.alertTitle);
+//        titleTextView.setText("Search Filter Options");
+//        // Create an EditText field programmatically
+//        final EditText editText = new EditText(getContext());
+//        editText.setHint("Enter search word here");
 
-        TextView titleTextView = customTitleView.findViewById(R.id.alertTitle);
-        titleTextView.setText("Search Filter Options");
+        View customDialogView = getLayoutInflater().inflate(R.layout.edit_search_parameters_dialog, null);
+        builder.setView(customDialogView);
 
+        ListView multiChoiceListView = customDialogView.findViewById(R.id.multiChoiceListView);
 
-        builder.setMessage("Options")
-//            .setMultiChoiceItems(new String[]{"test","test2"}, null, new DialogInterface.OnMultiChoiceClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-//
-//                }
-//            })
-            .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    // User cancels the dialog.
-                }
-            })
-           .setNegativeButton("Save", new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   // User cancels the dialog.
-               }
-           });
-
-        // Create an EditText field programmatically
-        final EditText editText = new EditText(getContext());
-        editText.setHint("Enter text here");
+        String[] providedItemsKeys = new String[localSearchOptionsHashMap.size()];
+//        providedItemsKeys = settingsStateMap.keySet().toArray();
 
 
-//        Somehow this aligns the positive to the right and the negative to the left.
-        builder.setPositiveButton("OK", null);
-        builder.setNegativeButton("Cancel", null);
+        multiChoiceListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        multiChoiceListView.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_multiple_choice, providedItemsKeys));
 
-        builder.setView(editText);
+        multiChoiceListView.setOnItemClickListener((parent, view, position, id) -> {
+            // Handle multi-choice item click
+            String key = providedItemsKeys[position];
+            localSearchOptionsHashMap.put(key, multiChoiceListView.isItemChecked(position));
+        });
+
+        // Find the EditText field and set its hint and text
+        EditText editText = customDialogView.findViewById(R.id.editText);
+        editText.setHint("Enter search word here");
+        editText.setText(persistentText);
+
+//        for (int j = 0; j < 2; j++) {
+//            multiChoiceListView.setItemChecked(j, true);
+//        }
+
+        int i = 0;
+        for(String key : localSearchOptionsHashMap.keySet()){ //Avoids giving access
+            providedItemsKeys[i] = key;
+            multiChoiceListView.setItemChecked(i, Boolean.TRUE.equals(localSearchOptionsHashMap.get(key)));
+            i++;
+        }
+
+        // Set title
+        builder.setTitle("Search Filter Options");
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener(){//Save (Button text set later)
+            public void onClick(DialogInterface dialog, int id){
+                //Save settings
+                viewModel.setSettingsStateMap(localSearchOptionsHashMap);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){//Cancel (Button text set later)
+            public void onClick(DialogInterface dialog, int id){
+                //Cancel
+
+            }
+        });
+
+
+
+
+
+////        Somehow this aligns the positive to the right and the negative to the left.
+//        builder.setPositiveButton("Save", null);
+//        builder.setNegativeButton("Cancel", null);
+
+//        builder.setView(editText);
 
         AlertDialog dialog = builder.create();
         return dialog;
